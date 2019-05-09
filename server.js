8c34c261cccf3a37cd2f0cccb343a35db7e1570a
unknown <malinduruwantha@gmail.com>
2019-05-09 22:07:21 +0530

    added node.js modules

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
-
+var date=require('./modules/datemodule')
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.write(date.myDateTime())
+  res.end();
+
 }).listen(8080);
