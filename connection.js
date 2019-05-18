8da6dfc1bdb2f281b7139ad3bcad2913d9a3beb0
unknown <malinduruwantha@gmail.com>
2019-05-18 10:48:01 +0530

    read data

--- connection.js
+++ connection.js
@@ -20,9 +20,14 @@ con.connect(function(err) {
 //     if (err) throw err;
 //     console.log("Table created");
 //   });
-var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-con.query(sql, function (err, result) {
-  if (err) throw err;
-  console.log("1 record inserted");
-});
+// var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+// con.query(sql, function (err, result) {
+//   if (err) throw err;
+//   console.log("1 record inserted");
+// });
+
+con.query("SELECT * FROM customers", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
   });
