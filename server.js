db8ae0574348921758c7067c821a0c873766294c
unknown <malinduruwantha@gmail.com>
2019-05-18 09:02:10 +0530

    req and res headers

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var date=require('./modules/datemodule')
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write(date.myDateTime())
+  res.writeHead(200,  {'Content-Type': 'text/html'});
+  res.write(date.myDateTime());
+  res.write(req.url);
   res.end();
 
 }).listen(8080);
